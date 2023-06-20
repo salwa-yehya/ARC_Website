75f9e487359c2099bf4c721efc4a14aae6dd30b6
salwa-yehya <salwayehyaabuafash@gmail.com>
2023-06-21 01:54:55 +0300

    vision

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -4,6 +4,7 @@ import Pageshead from '../componant/Pageshead'
 import Footer from '../componant/Footer'
 import Menu from '../componant/Menu'
 import Vision from '../componant/Vision'
+import OurVisionSection from '../componant/OurVisionSection'
 
 const AboutPage = () => {
   return (
@@ -11,6 +12,7 @@ const AboutPage = () => {
           <Menu/>
           <Pageshead pageTitle="about us" description="The Most Important Thing To Us Is Satisfy Our Customers .."/> 
           <Vision/> 
+          <OurVisionSection/>
           <Team/>
           <Footer/>
     </div>
--- src/componant/OurVisionSection.jsx
+++ src/componant/OurVisionSection.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-
+import './OurVisionSection.css'
 const OurVisionSection = () => {
     return (
         <div id='vision-sectiion'>
             <div className="vision-text">
                 <h2>Our Vision</h2>
                 <p>
-                At ARC, our vision is to transform the digital landscape by providing businesses with unparalleled solutions that drive growth, enhance user experiences, and foster long-lasting success. We strive to stay at the forefront of technology, enabling our clients to stay ahead of the curve and consistently outperform their competitors.
+                    At ARC, our vision is to transform the digital landscape by providing businesses with unparalleled solutions that drive growth, enhance user experiences, and foster long-lasting success. We strive to stay at the forefront of technology, enabling our clients to stay ahead of the curve and consistently outperform their competitors.
                 </p>
-
             </div>
             <div className="vision-image">
-                <img src="./images/teamabout.jpg" alt="" />
+                <img src="./images/vision1.jpg" alt="" />
             </div>
         </div>
     )
